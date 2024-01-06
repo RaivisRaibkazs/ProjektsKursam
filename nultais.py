a62bf4cb2af1b6e5547e89a0238266e71081811f
RaivisRaibkazs <144050078+RaivisRaibkazs@users.noreply.github.com>
2024-01-06 17:54:41 +0200

    Update nultais.py
    pagaidu uzlabojums

--- nultais.py
+++ nultais.py
@@ -1,3 +1,20 @@
+import selenium
 from selenium import webdriver
-driver = webdriver.Edge()
-driver.get("https://www.y8.com")
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+import time
+
+try:
+    service = Service()
+    option = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=option)
+    url = "https://www.y8.com"
+    driver.get(url)
+    time.sleep(3)
+
+    find=driver.find_element(By.ID," css-ik5ir8")
+    print(find)
+
+except Exception as e:
+    print(f"Error: {e}")
