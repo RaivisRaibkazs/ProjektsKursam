677efc8a06189971aa57cd71ddc842263d7cae6e
RaivisRaibkazs <144050078+RaivisRaibkazs@users.noreply.github.com>
2024-01-09 18:54:22 +0200

    Update nultais.py
    pareja uz Edge, ka ari pirma plana punkta gals.

--- nultais.py
+++ nultais.py
@@ -1,20 +1,25 @@
-import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
-try:
-    service = Service()
-    option = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=service, options=option)
-    url = "https://www.y8.com"
-    driver.get(url)
-    time.sleep(3)
+driver  = webdriver.Edge(executable_path=r"C:\Windows\msedgedriver.exe")
 
-    find=driver.find_element(By.ID," css-ik5ir8")
-    print(find)
+URL = "https://www.y8.com"
+driver.get(URL)
 
-except Exception as e:
-    print(f"Error: {e}")
+wait = WebDriverWait(driver, 5)
+agree = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "css-ik5ir8")))
+agree.click()
+got_it = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "validate-policy")))
+got_it.click()
+search = wait.until(EC.element_to_be_clickable((By.ID, "q")))
+search.send_keys("Sinjid")
+findgame = wait.until(EC.element_to_be_clickable((By.ID, "ui-id-2")))
+findgame.click()
+download_game = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "download-link")))
+download_game.click()
+time.sleep(20)
+
+driver.quit()
